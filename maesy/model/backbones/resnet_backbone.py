Added SWIN Backbone option. (And fixed backprop instability by detaching reference_boxes before inverse sigmoid)

maesy/model/backbones/resnet_backbone.py
@@ -8,6 +8,15 @@ from torchvision.models import resnet50, ResNet50_Weights, resnet18, ResNet18_We
 
 @dataclass
 class ResNetBackboneConfig:
+    """
+        Config class for ResNet backbones
+
+        Args:
+            :param version: ResNet version ('resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152')
+            :param image_size: Input image size (assumed square)
+            :param pretrained: Whether to use pre-trained weights
+            :param feature_scales: Specify which feature scale levels to calculate and return during forward pass
+        """
     version: str = "resnet50"
     image_size: int = 224
     pretrained: bool = True
@@ -21,9 +30,7 @@ class ResNetBackbone(nn.Module):
         Initialize ResNet backbone.
 
         Args:
-            :param version: ResNet version ('resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152')
-            :param image_size: Input image size (assumed square)
-            :param remove_layers: Number of layers to remove from the end (default: 1, removes the classification layer but keeps global average pooling)
+            :param config: ResNetBackboneConfig class that holds all necessary parameters
         """
         super().__init__()
         self.config = config
@@ -47,14 +54,12 @@ class ResNetBackbone(nn.Module):
             "resnet152": resnet152,
         }
         if config.version not in constructors:
-            raise ValueError(f"Unsupported ResNet version for RT-DETR: {config.version}")
+            raise ValueError(f"Unsupported ResNet version: {config.version}")
 
         self.calc_c3 = "c3" in self.config.feature_scales or "c4" in self.config.feature_scales or "c5" in self.config.feature_scales or "c6" in self.config.feature_scales
         self.calc_c4 = "c4" in self.config.feature_scales or "c5" in self.config.feature_scales or "c6" in self.config.feature_scales
         self.calc_c5 = "c5" in self.config.feature_scales or "c6" in self.config.feature_scales
         self.calc_c6 = "c6" in self.config.feature_scales
-
-        # self.return_idx = sorted([int(scale[1])-3 for scale in self.config.feature_scales]) # indices to select which feature_scales are returned
 
         self.feature_dim: Dict[str, int] = {"c3":128, "c4":256, "c5":512} if self.config.version in {"resnet18", "resnet34"} else {"c3":512, "c4":1024, "c5":2048}
 

maesy/model/heads/rt_detr_head.py
@@ -303,7 +303,7 @@ class RTDETRHead(nn.Module):
     @staticmethod
     def _inverse_sigmoid(x: torch.Tensor, eps: float = 1e-5) -> torch.Tensor:
         x = x.clamp(min=eps, max=1.0 - eps)
-        return torch.log(x / (1.0 - x))
+        return torch.log(x) - torch.log(1.0 - x) # Division-safe version of torch.log(x / (1.0 - x))
 
     def _build_positional_encoding(self, feat: torch.Tensor) -> torch.Tensor:
         b, c, h, w = feat.shape
@@ -446,6 +446,8 @@ class RTDETRHead(nn.Module):
         memory: torch.Tensor,
         spatial_shapes: List[Tuple[int, int]],
     ) -> Dict[str, List[torch.Tensor]]:
+        reference_boxes = reference_boxes.detach() # Detach from gradient graph due to instability issues.
+
         reference_logits = self._inverse_sigmoid(reference_boxes)
         line_reference_logits = self._inverse_sigmoid(reference_boxes)
 

maesy/training/base_trainer.py
@@ -195,12 +195,12 @@ class BaseTrainer(ABC):
             if self.config.use_amp:
                 self.scaler.scale(loss).backward()
                 self.scaler.unscale_(self.optimizer)
-                torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.config.max_grad_norm)
+                total_norm = torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.config.max_grad_norm)
                 self.scaler.step(self.optimizer)
                 self.scaler.update()
             else:
                 loss.backward()
-                torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.config.max_grad_norm)
+                total_norm = torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.config.max_grad_norm)
                 self.optimizer.step()
 
             # Update progress bar
@@ -215,6 +215,7 @@ class BaseTrainer(ABC):
                 data = {f"train/{k}": v.item() for k, v in losses.items() if not k.startswith('img_')}
                 data['train/lr'] = self.optimizer.param_groups[-1]['lr']
                 data['train/lr_bbone'] = self.optimizer.param_groups[0]['lr']
+                data['train/gradient_norm'] = total_norm.item()
                 if self.enable_wandb: self.wandb_run.log(data=data, step=self.global_step, commit=True)
 
             self.global_step += 1
@@ -267,6 +268,8 @@ class BaseTrainer(ABC):
         print(f"Training samples: {len(self.train_loader.dataset)}")
         if self.val_loader:
             print(f"Validation samples: {len(self.val_loader.dataset)}")
+
+        # torch.autograd.set_detect_anomaly(True) # TODO: Make opt-in (as this slows down training massively)
 
         for epoch in range(self.current_epoch, self.config.num_epochs):
             self.current_epoch = epoch

maesy/training/losses.py
@@ -382,7 +382,7 @@ class DetectionLoss(BaseLoss):
 
         batch_size, num_queries = pred_logits.shape[:2]
         # Flatten to compute cost matrices
-        out_prob = pred_logits.flatten(0, 1).softmax(-1)  # [B*num_queries, num_classes + 1]
+        out_prob = pred_logits.float().flatten(0, 1).log_softmax(-1)  # [B*num_queries, num_classes + 1]
         out_bbox = pred_boxes.flatten(0, 1)  # [B*num_queries, 4]
         indices = []
 
@@ -433,10 +433,16 @@ class DetectionLoss(BaseLoss):
             if cost_giou.shape[1] > cost_giou.shape[0]:
                 raise ValueError(f"Hungarian matching failed, more target boxes than predictions. Cost GIoU shape: {cost_giou.shape}")
 
-            # print(f"Cost matrices for batch item {i}:")
-            # print(cost_class.mean())
-            # print(cost_bbox.mean())
-            # print(cost_giou.mean())
+            def _check(name, x):
+                if not torch.isfinite(x).all():
+                    print(f"\n🚨 {name} has NaNs/Infs")
+                    print("min:", x.min().item())
+                    print("max:", x.max().item())
+                    raise RuntimeError(name)
+
+            _check("cost_class", cost_class)
+            _check("cost_bbox", cost_bbox)
+            _check("cost_giou", cost_giou)
 
             # Final cost matrix
             C = self.bbox_loss_coef * cost_bbox + self.line_loss_coef * cost_line + self.class_loss_coef * cost_class + self.giou_loss_coef * cost_giou
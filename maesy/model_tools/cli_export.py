f6e674b235431446b0a336c7aac7306d4024098e
simon <simon.richter@tu-dortmund.de>
2026-05-19 11:45:46 +0200

    Repaired model export to onnx

--- maesy/model_tools/cli_export.py
+++ maesy/model_tools/cli_export.py
@@ -5,4 +5,4 @@ def main(args):
     """
     Handle the 'export' command to export a trained model to a different format (e.g. ONNX).
     """
-    export_vit_detector(checkpoint_path=args.checkpoint, output_path=args.out),
+    export_vit_detector(model_info=args.model, output_path=args.outputpath, name=args.name, num_classes=args.num_classes, enable_line_detection=args.enable_line_detection, enable_ellipse_detection=args.enable_ellipse_detection, line_class_id=args.line_class_id, ellipse_class_id=args.ellipse_class_id)
--- maesy/training/train_setups/train_object_detection.py
+++ maesy/training/train_setups/train_object_detection.py
@@ -136,7 +136,7 @@ def train_vit_detector(
 
     # Create training configuration
     training_config = ODTrainingConfig(
-        batch_size=64,
+        batch_size=48, # 64, # TODO: Everything apart from largest model config (rt-detr6) was with 64
         num_epochs=750,
         learning_rate=1e-4 if freeze else 1e-4,  # Higher LR when only training head
         backbone_learning_rate=0.0 if freeze else 1e-5,  # Lower LR for backbone if fine-tuning, otherwise 0
@@ -204,10 +204,10 @@ def train_vit_detector(
         else:
             config["enable_ellipse_detection"] = False
         model = create_model_from_config(config)
-    elif not model_info.endswith(".pth"):
-        raise ValueError(f"Model {model_info} is neither in {known_architectures} nor is it a path to a training checkpoint (must end with '.pth')")
-    else:
+    elif model_info.endswith(".pth"):
         model = create_model_from_checkpoint(model_info)
+    else:
+        raise ValueError(f"Model {model_info} is neither in {known_architectures} nor is it a path to a training checkpoint (must end with '.pth')")
 
     if pretrained_backbone != "":
         print(f"Loading pretrained backbone weights from {pretrained_backbone}...")
@@ -311,29 +311,62 @@ def infer_vit_detector(
         visualize_data(str(out_path), "")
 
 def export_vit_detector(
-    checkpoint_path: str,
+    model_info: str,
     output_path: str,
+    name: str,
+    num_classes: int = -1,
+    enable_line_detection: bool = False,
+    enable_ellipse_detection: bool = False,
+    line_class_id: int = -1,
+    ellipse_class_id: int = -1,
 ) -> None:
     """
     Export a trained object detection model to ONNX format for deployment.
 
     Args:
-        :param checkpoint_path: Path to trained model checkpoint
+        :param model_info: String that either specifies a model architecture or the path to a trained checkpoint
         :param output_path: Path to save the exported ONNX model
         # :param detector_arch: Architecture of the model. If None, the architecture will be inferred from the checkpoint. (e.g., "detr" or "rt_detr")
     """
 
-    model = create_model_from_checkpoint(checkpoint_path)
+    if model_info.lower() in known_architectures:
+        assert num_classes != -1 and (line_class_id != -1 or not enable_line_detection) and (ellipse_class_id != -1 or not enable_ellipse_detection) and output_path != "", f"If using an architecture specifier, additional input is required"
+        config = read_yaml(f"cfg/{model_info.lower()}.yaml")
+        if config["num_classes"] != -1 and config["num_classes"] != num_classes:
+            raise ValueError("num_classes parameter in model config does not match the datasets 'nc' parameter. Leave value in config on '-1' to enable auto-detect.")
+        config["num_classes"] = num_classes
+        if enable_line_detection:
+            if config["line_class_id"] != -1 and config["line_class_id"] != line_class_id:
+                raise ValueError(
+                    f"line_class_id parameter in model config ({config['line_class_id']}) does not match the datasets line_class_id parameter ({line_class_id}). Leave value in config on '-1' to enable auto-detect.")
+            config["line_class_id"] = line_class_id
+            config["enable_line_detection"] = True
+        else:
+            config["enable_line_detection"] = False
+        if enable_ellipse_detection:
+            if config["ellipse_class_id"] != -1 and config["ellipse_class_id"] != ellipse_class_id:
+                raise ValueError(
+                    f"ellipse_class_id parameter in model config ({config['ellipse_class_id']}) does not match the datasets ellipse_class_id parameter ({ellipse_class_id}). Leave value in config on '-1' to enable auto-detect.")
+            config["ellipse_class_id"] = ellipse_class_id
+            config["enable_ellipse_detection"] = True
+        else:
+            config["enable_ellipse_detection"] = False
+        model = create_model_from_config(config)
+    elif model_info.endswith(".pth"):
+        model = create_model_from_checkpoint(model_info)
+    else:
+        raise ValueError(f"Model {model_info} is neither in {known_architectures} nor is it a path to a training checkpoint that ends with '.pth'")
+
     model.eval()
 
     example_inputs = (torch.randn(1, 3, 224, 224),)
     onnx_program = torch.onnx.export(model, example_inputs, dynamo=True)
 
-    path = Path(checkpoint_path).parent if output_path == "" else Path(output_path)
+    path = Path(model_info).parent if output_path == "" else Path(output_path)
     path.mkdir(parents=True, exist_ok=True)
     print("=" * 60)
     print("Starting ONNX export...")
-    save_name = path / f"{model.config.type}.onnx"
+    save_name = path / (f"{model.config.type}.onnx" if name == "" else f"{name}.onnx")
     onnx_program.save(save_name)
     print("=" * 60)
     print(f"Success! Model has been exported to {save_name}")
